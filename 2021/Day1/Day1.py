d4b2ba3f924d57911a15e941a4b5df62711aadea
hughjph <hughjohnson2018@outlook.com>
2021-12-01 17:35:57 +0000

    refactor

--- 2021/Day1/Day1.py
+++ 2021/Day1/Day1.py
@@ -9,6 +9,20 @@ print(lines)
 
 number_increased = 0
 
+#Part 1
+
+for i in range(2, len(lines)):
+    if lines[i]>lines[i-1]:
+        number_increased += 1
+    
+
+
+
+print("Part 1")
+print(number_increased)
+
+#Part 2
+number_increased = 0
 previous_sum = 0
 
 for i in range(2, len(lines)):
@@ -16,4 +30,5 @@ for i in range(2, len(lines)):
         number_increased += 1
     previous_sum = lines[i]+lines[i-1]+lines[i-2]
 
+print("Part 2")
 print(number_increased-1)
