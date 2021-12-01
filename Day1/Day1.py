0c6fe278fa0079a5551d9519b8348186e8873d75
hughjph <hughjohnson2018@outlook.com>
2021-12-01 16:08:20 +0000

    Day1

---
+++ Day1/Day1.py
@@ -0,0 +1,19 @@
+file = open("Day1\Day1Data.txt", "r")
+lines = file.readlines()
+
+for i in range(len(lines)):
+    lines[i] = lines[i].replace('\n', '')
+    lines[i] = int(lines[i])
+    
+print(lines)
+
+number_increased = 0
+
+previous_sum = 0
+
+for i in range(2, len(lines)):
+    if (lines[i]+lines[i-1]+lines[i-2])>previous_sum:
+        number_increased += 1
+    previous_sum = lines[i]+lines[i-1]+lines[i-2]
+
+print(number_increased-1)
