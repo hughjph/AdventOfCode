fdeecb9d596003c58acf8fd51c64d65429f76596
Hugh Johnson <hughjohnson2018@outlook.com>
2021-12-06 08:23:34 +0000

    what is this

--- 2021/Day4/part-1.py
+++ 2021/Day4/part-1.py
@@ -9,7 +9,7 @@ file.close()
 
 print(lines[0])
 
-lines = lines.pop(0)
+numbers = lines[0].split(",")
 
 data = [
 
@@ -24,3 +24,13 @@ while x < 601:
         row = lines[i+x].split(" ")
         sheet.update({"row" + i : "row"})
     data.append(sheet)
+    x+=6
+
+print(data[10])
+
+for i in range(len(numbers)):
+    number = numbers[i]
+    for j in range(len(data)):
+        for k in range(5):
+            
+
